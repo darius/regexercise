4143f3b4aece0c29e84a4851a5815a9ad2146839
Darius Bacon <darius@wry.me>
2013-07-09 11:40:36 -0700

    refactor to take a function param, etc.

--- testcases.py
+++ testcases.py
@@ -4,23 +4,29 @@ Tests for the exercises.
 I'll assume you supply functions named problem1(), problem2(),...
 """
 
-def check1(patterns, string, remainder):
-    stream = iter(string)
-    result = problem1(patterns, stream)
-    if remainder is None:
-        assert result is None
-        for ch in stream:
-            assert False, "Stream should be exhausted"
-    else:
-        assert result is not None
-        assert remainder == ''.join(stream)
+def check1(problem1):
 
-check1([], '', None)
-check1([], 'wheee', None)
-check1([''], 'wheee', 'wheee')
-check1(['X'], 'wheee', None)
-check1(['X'], 'X', '')
-check1(['X'], 'wheXXXee', 'Xee')
-check1(['X'], 'yX', '')
-check1(['X', 'X'], 'yX', '')
-check1(['X', 'Y'], 'YX', '')
+    def case1(pattern, string, remainder):
+        check_patterns(pattern.split('|'), string, remainder)
+
+    def check_patterns(patterns, string, remainder):
+        stream = iter(string)
+        result = problem1(patterns, stream)
+        if remainder is None:
+            assert result is None
+            for ch in stream:
+                assert False, "Stream should be exhausted"
+        else:
+            assert result is not None
+            assert remainder == ''.join(stream)
+
+    check_patterns([], '', None)
+    check_patterns([], 'wheee', None)
+    case1('', 'wheee', 'wheee')
+    case1('X', 'wheee', None)
+    case1('X', 'X', '')
+    case1('X', 'wheXXXee', 'Xee')
+    case1('X', 'yX', '')
+    case1('X|X', 'yX', '')
+    case1('X|YY', 'YX', '')
+    case1(r'rat|cat', 'a cat is fat', ' is fat')
