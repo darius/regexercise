cc110be828270b82b5c2a85ac10e938780213f85
Darius Bacon <darius@wry.me>
2013-07-19 17:59:07 -0700

    add main() to tests. better failure reporting.

--- tests.py
+++ tests.py
@@ -4,20 +4,34 @@ Tests for the exercises.
 
 from regex_parse import make_parser
 
+def main(argv):
+    problems = 'literals abstract_data repetition zero_or_more'.split()
+    if 1 < len(argv):
+        problems = argv[1:]
+    for problem in problems:
+        print('Testing ' + problem + '.py: ')
+        globals()['check_'+problem](__import__(problem))
+        print('ok.')
+    return 0
+
 def check_search(search, pattern, string, remainder):
+    correct_result = remainder is not None
     stream = iter(string)
     result = search(pattern, stream)
     unconsumed = ''.join(stream)
-    test_case = ''.join('\n %s %r' % pair
-                        for pair in (("Pattern: ", pattern),
-                                     ("Input:", string),
-                                     ("Remainder should be:", remainder),
-                                     ("But remainder is:   ", unconsumed)))
+    pairs = (("Pattern: ", pattern), ("Input:", string))
+    if result is not correct_result:
+        pairs += (("Result should be:", correct_result),
+                  ("But result is:   ", result))
+    if remainder != unconsumed:
+        pairs += (("Remainder should be:", remainder),
+                  ("But remainder is:   ", unconsumed))
+    test_case = ''.join('\n %s %r' % pair for pair in pairs)
     if remainder is None:
-        assert result is False, "The result should be False." + test_case
+        assert result is False, "Wrong result." + test_case
         assert not unconsumed, "The stream should be exhausted." + test_case
     else:
-        assert result is True, "The result should be True." + test_case
+        assert result is True, "Wrong result." + test_case
         assert unconsumed == remainder, "Too much or too litle of the stream was consumed." + test_case
 
 def check(module, regex_string, string, remainder):
@@ -86,3 +100,7 @@ def check_zero_or_more(module):
     check(module, r'a(b*)*d', 'an ad attacks', ' attacks')
     check(module, r'a(b*)*d', 'an abdomen', 'omen')
     return "Zero or more: all tests passed."
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
