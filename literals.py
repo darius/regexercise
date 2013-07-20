1c6ab452e893e5596f1e23e6795bef30e7968849
Darius Bacon <darius@wry.me>
2013-07-20 16:15:49 -0700

    document stubs

--- literals.py
+++ literals.py
@@ -1,2 +1,9 @@
+"""
+Fill in this stub.
+"""
+
 def search(strings, chars):
+    """Given a sequence of strings and an iterator of chars, return True
+    if any of the strings would be a substring of ''.join(chars); but
+    only consume chars up to the end of the match."""
     raise NotImplementedError
--- repetition.py
+++ repetition.py
@@ -1,7 +1,16 @@
+"""
+Fill in this stub.
+"""
+
 def search(re, chars):
+    """Given a regular expression and an iterator of chars, return True
+    if re matches some substring of ''.join(chars); but only consume
+    chars up to the end of the match."""
     raise NotImplementedError
 
-def literal(char): return ('literal', char, None)
-def chain(r, s):   return ('chain', r, s)
-def either(r, s):  return ('either', r, s)
-def plus(r):       return ('plus', r, None)
+# Regular expression constructors; the re above is built by these.
+# Feel free to change these definitions if it helps.
+def literal(char):    return ('literal', char, None)
+def chain(re1, re2):  return ('chain', re1, re2)
+def either(re1, re2): return ('either', re1, re2)
+def plus(re):         return ('plus', re, None)
