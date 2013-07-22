6337b973d2544e40ca647602134b01a820ef5b8b
Darius Bacon <darius@wry.me>
2013-07-22 11:21:56 -0700

    change name to run()

--- hal_watch.py
+++ hal_watch.py
@@ -5,7 +5,7 @@ See step-by-step running of the vm. Hit enter at each step.
 import ansi
 import hal_vm
 
-def watch(filename, inputs):
+def run(filename, inputs):
     vm = hal_vm.load(filename, inputs)
     while True:
         sys.stdout.write(ansi.clear_screen)
@@ -19,4 +19,4 @@ def watch(filename, inputs):
 
 if __name__ == '__main__':
     import sys
-    watch(sys.argv[1], sys.argv[2])
+    run(sys.argv[1], sys.argv[2])
