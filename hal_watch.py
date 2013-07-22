156ee2ef157f382c97888df73d6bcc792c6ad8fd
Darius Bacon <darius@wry.me>
2013-07-22 11:19:58 -0700

    move hal stuff into main  dir to be easier to run

--- hal_watch.py
+++ hal_watch.py
@@ -2,18 +2,18 @@
 See step-by-step running of the vm. Hit enter at each step.
 """
 
-import ansi                     # from ../../editor/
-import halvm
+import ansi
+import hal_vm
 
 def watch(filename, inputs):
-    vm = halvm.load(filename, inputs)
+    vm = hal_vm.load(filename, inputs)
     while True:
         sys.stdout.write(ansi.clear_screen)
         vm.show()
         sys.stdin.read(1)
         try:
             vm.step()
-        except halvm.Halt, e:
+        except hal_vm.Halt, e:
             print e
             break
 
